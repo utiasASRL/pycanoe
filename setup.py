59c14f1933874e4ef186d9550a939c7170715934
mia-e-thomas <mia.thomas@robotics.utias.utoronto.ca>
2025-10-22 21:45:58 +0000

    Basic repo files

---
+++ setup.py
@@ -0,0 +1,29 @@
+import setuptools
+from pathlib import Path
+
+this_directory = Path(__file__).parent
+with open(str(this_directory / "README.md"), encoding="utf-8") as f:
+    long_description = f.read()
+
+setuptools.setup(
+    name="asrl-pycanoe",
+    version="0.1.0",
+    description="A toolkit for working with the Canoe dataset in Python",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    author="Blerim Abdullai, Yubo Wang, Mia Thomas",
+    # TODO: Set up email
+    # TODO: Set up URL
+    # author_email='canoe@robotics.utias.utoronto.ca',
+    # url='https://github.com/utiasASRL/canoe-devkit',
+    license="BSD",
+    packages=setuptools.find_packages(),
+    # TODO: Set up dependencies
+    # python_requires='>=3.8',
+    # install_requires=[
+    # 	"numpy>=1.21.0",
+    # 	"opencv-python>=4.5.3.56",
+    # 	"matplotlib>=3.4.2",
+    # 	"asrl-pysteam>=1.1.0",
+    # ]
+)

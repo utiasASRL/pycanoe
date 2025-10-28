34b5d316dfa54befcf7af8f0ff750962942f8ffe
mia-e-thomas <mia.thomas@robotics.utias.utoronto.ca>
2025-10-27 22:40:35 -0400

    Cam-lidar calibration visualization script update

--- scripts/view_cam_lidar_calib.py
+++ scripts/view_cam_lidar_calib.py
@@ -1,38 +1,40 @@
 import numpy as np
-
-# import matplotlib.pyplot as plt
-from pycanoe.data.sensors import Lidar, Camera
-from pycanoe.utils.vis_utils import bilinear_interp
+import os
 import open3d as o3d
 import matplotlib.pyplot as plt
 
-# path_lid = "/home/otter/otter_data_processed/canoe/canoe-2025-08-21-19-00/lidar/1755802821364780.bin"
-# path_cam = "/home/otter/otter_data_processed/canoe/canoe-2025-08-21-19-00/cam_left/1755802821315769.png"
-
-# path_lid = "/home/otter/otter_data_processed/canoe/canoe-2025-08-21-19-00/lidar/1755802844464735.bin"  # 44.464
-# path_cam = "/home/otter/otter_data_processed/canoe/canoe-2025-08-21-19-00/cam_left/1755802844418450.png"  # 44.418
-# path_cam_2 = "/home/otter/otter_data_processed/canoe/canoe-2025-08-21-19-00/cam_left/1755802844518383.png"  # 44.518
-
-# path_lid = "/home/otter/otter_data_processed/canoe/extract_calib/canoe-2025-08-28-23-52/lidar/1756425170526283.bin"
-# path_cam = "/home/otter/otter_data_processed/canoe/extract_calib/canoe-2025-08-28-23-52/cam_left/1756425170546748.png"
-
-path_lid = "/home/otter/otter_data_processed/canoe/canoe-2025-08-21-11-31/lidar/1755775928359788.bin"  # 08.359
-path_cam = "/home/otter/otter_data_processed/canoe/canoe-2025-08-21-11-31/cam_left/1755775928307903.png"  # 08.307
+from pycanoe.data.sensors import Lidar, Camera
+from pycanoe.utils.utils import get_time_from_filename, get_closest_index
+from pycanoe.utils.vis_utils import bilinear_interp
 
 
-path_lid_save = "/home/otter/otter_data_processed/canoe/calib/lidar_overlay.png"
-path_cam_save = "/home/otter/otter_data_processed/canoe/calib/cam_overlay.png"
+# ----- DATA PATHS ----- #
+cam_paths = [
+    "~/otter_data_processed/canoe/canoe-2025-08-21-19-00/cam_left/1755802821315769.png",  # Boat 1a
+    "~/otter_data_processed/canoe/canoe-2025-08-21-19-00/cam_left/1755802821415810.png",  # Boat 1b
+    "~/otter_data_processed/canoe/canoe-2025-08-21-19-00/cam_left/1755802821515924.png",  # Boat 1c
+    "~/otter_data_processed/canoe/canoe-2025-08-21-19-00/cam_left/1755802844418450.png",  # Boat 2
+    "~/otter_data_processed/canoe/extract_calib/canoe-2025-08-28-23-52/cam_left/1756425170546748.png",  # Blerim Calib
+    "~/otter_data_processed/canoe/canoe-2025-08-21-11-31/cam_left/1755775928307903.png",  # Shore
+]
 
+# ----- SAVE PATHS ----- #
+save_path = os.path.expanduser(
+    "~/otter_data_processed/calibration_info/visualizations/"
+)
+
+# region# ----- TRANSFORM PATHS ----- #
 T_sl_path = "/home/otter/otter_data_processed/calibration_info/T_sensor_lidar.txt"
 # Updated uses the bumblebee intrinsics
 T_cl_path = "/home/otter/otter_data_processed/calibration_info/T_cam_lidar-updated.txt"
 P_c_path = "/home/otter/otter_data_processed/calibration_info/P_cam_left-updated.txt"
+# endregion
+
+# region # ----- VIS FUNCTIONS ----- #
 
 
-# ----- VIS FUNCTIONS ----- #
-# region
 def vis_points_on_image(
-    img, uv, colors, im_show=True, color_bar=True, save=None, **kwargs
+    img, uv, colors, color_bar=True, place_text=None, show=True, save=None, **kwargs
 ):
     # Params
     height, width = img.shape[0:2]
@@ -49,8 +51,7 @@ def vis_points_on_image(
     # Create figure
     fig = plt.figure(figsize=(width / 100, height / 100), dpi=100)
     ax = fig.add_subplot()
-    if im_show:
-        ax.imshow(img)
+    ax.imshow(img)
     ax.set_xlim(0, width)
     ax.set_ylim(0, height)
     sc = ax.scatter(
@@ -61,11 +62,23 @@ def vis_points_on_image(
     )
     ax.set_axis_off()
     ax.invert_yaxis()
+    if place_text is not None:
+        plt.text(
+            0.01,
+            0.99,
+            place_text,
+            color="#800020",
+            fontsize=18,
+            ha="left",
+            va="top",
+            transform=plt.gca().transAxes,
+        )
     if color_bar:
         plt.colorbar(sc, ax=ax, fraction=0.03, pad=0)
     if save is not None:
         plt.savefig(save, bbox_inches="tight")
-    plt.show()
+    if show:
+        plt.show()
 
 
 def vis_lidar_o3d(lidar, colors=None, coord_frame=False, **kwargs):
@@ -94,58 +107,75 @@ if __name__ == "__main__":
     max_depth = 6.0
     intensity_thresh = 0
 
-    # ----- LOAD TRANSFORMATIONS & DATA ----- #
-    # region
+    # region# ----- LOAD TRANSFORMATIONS ----- #
     T_cam_lid = np.loadtxt(T_cl_path)
     T_sens_lid = np.loadtxt(T_sl_path)
     T_sens_lid[0:3, 3] *= 0.001  # m -> mm
     P_cam = np.loadtxt(P_c_path)
-
-    # Data
-    lidar = Lidar(path_lid)
-    lidar.load_data()
-
-    camera = Camera(path_cam)
-    camera.load_data()
     # endregion
 
-    # ----- TRANSFORM & FILTER DEPTH AND INTENSITY ----- #
-    # region
-    # Transform: sensor -> lidar.
-    lidar.transform(np.linalg.inv(T_sens_lid))
-    # Transform: lidar -> camera.
-    lidar.transform(T_cam_lid)
+    for cam_idx, cam_path in enumerate(cam_paths):
 
-    # Filter for region of interest [xmin, xmax, ymin, ymax, zmin, zmax]
-    # TODO: make all bounds customizable
-    # bounds = []
-    bounds = [-75, 75, -20, 20, 0.5, max_depth]
-    # bounds = [-75, 75, -20, 20, 2, 50]
-    lidar.passthrough(bounds)
+        # region# ----- LOAD DATA ----- #
+        cam_path = os.path.expanduser(cam_path)
+        camera = Camera(cam_path)
+        camera.load_data()
 
-    # Mask w/ intensity
-    if intensity_thresh:
-        lidar.points = lidar.points[(lidar.points[:, 3] > intensity_thresh)]
-    # endregion
+        # Load closest lidar
+        lidar_root = os.path.join(camera.seq_root, "lidar")
+        lidar_files = os.listdir(lidar_root)
+        lidar_times = [get_time_from_filename(name) for name in lidar_files]
+        lidar_times, lidar_files = zip(*sorted(zip(lidar_times, lidar_files)))
+        closest = get_closest_index(camera.timestamp, lidar_times)
+        path_lid = os.path.join(lidar_root, lidar_files[closest])
 
-    # ----- LIDAR-IMAGE CORRESPONDENCES ----- #
-    # Project onto image
-    uv, proj_colors, uv_mask = lidar.project_onto_image(P_cam)
-    uv_colors = bilinear_interp(camera.img, uv[:, 0], uv[:, 1]) / 255.0
+        lidar = Lidar(path_lid)
+        lidar.load_data()
 
-    # ----- VISUALIZE ----- #
-    # 0) Transform: camera -> lidar
-    lidar.transform(np.linalg.inv(T_cam_lid))
+        # Print time diff
+        time_diff = abs(camera.timestamp - lidar.timestamp)
+        print(f"Time Diff Pair {cam_idx}: {time_diff}")
+        # endregion
 
-    # 1) Lidar Points on Image w/ Colorized Depth
-    vis_points_on_image(camera.img, uv, proj_colors, save=path_cam_save)
-    # vis_points_on_image(camera.img, uv, uv_colors, im_show=False, color_bar=False, s=50)
+        # region# ----- TRANSFORM & FILTER DEPTH AND INTENSITY ----- #
+        # Transform: sensor -> lidar.
+        lidar.transform(np.linalg.inv(T_sens_lid))
+        # Transform: lidar -> camera.
+        lidar.transform(T_cam_lid)
 
-    if False:
-        lidar.visualize(
-            fig_size=(15, 15), color_vec=uv_colors, azim_delta=0, elev_delta=0
+        # Filter for region of interest [xmin, xmax, ymin, ymax, zmin, zmax]
+        # TODO: make all bounds customizable
+        # bounds = []
+        bounds = [-75, 75, -20, 20, 0.5, max_depth]
+        # bounds = [-75, 75, -20, 20, 2, 50]
+        lidar.passthrough(bounds)
+
+        # Mask w/ intensity
+        if intensity_thresh:
+            lidar.points = lidar.points[(lidar.points[:, 3] > intensity_thresh)]
+        # endregion
+
+        # ----- LIDAR-IMAGE CORRESPONDENCES ----- #
+        # Project onto image
+        uv, proj_colors, uv_mask = lidar.project_onto_image(P_cam)
+        uv_colors = bilinear_interp(camera.img, uv[:, 0], uv[:, 1]) / 255.0
+
+        # ----- VISUALIZE ----- #
+        # 0) Transform: camera -> lidar
+        lidar.transform(np.linalg.inv(T_cam_lid))
+
+        info_string = f"Time Diff: {time_diff:.4f}s\nMax Depth: {max_depth:.1f}m"
+        # 1) Lidar Points on Image w/ Colorized Depth
+        vis_points_on_image(
+            camera.img,
+            uv,
+            proj_colors,
+            place_text=info_string,
+            show=False,
+            save=os.path.join(save_path, "overlay-" + str(cam_idx) + ".png"),
         )
+        # vis_points_on_image(camera.img, uv, uv_colors, im_show=False, color_bar=False, s=50)
 
-    # ----- OFFLOAD DATA ----- #
-    lidar.unload_data()
-    camera.unload_data()
+        # ----- OFFLOAD DATA ----- #
+        lidar.unload_data()
+        camera.unload_data()

5053d216aca5045902d243152caaf738d5180da6
mia-e-thomas <mia.thomas@robotics.utias.utoronto.ca>
2025-12-08 21:58:45 -0500

    dashboard vis update

--- scripts/test_loader.py
+++ scripts/test_loader.py
@@ -4,48 +4,18 @@ from datetime import datetime, timezone, timedelta
 import matplotlib
 import matplotlib.pyplot as plt
 import numpy as np
-from concurrent.futures import ProcessPoolExecutor
+from concurrent.futures import ThreadPoolExecutor as Pool
 
 from pycanoe.data.sequence import Sequence
 
 matplotlib.use("Agg")
 
-UTC_TO_EDT_OFFSET = -5
+UTC_TO_LOCAL = -5
 
 canoe_path = "/home/otter/otter_data_processed/canoe/"
 seq_id = "canoe-2025-08-21-19-00"
 seq_start = str(int(1755802837617698 + 120 * 1e6))
 seq_end = str(int(1755802837617698 + 130 * 1e6))  # 30 seconds
-
-
-def write_cam_left_video(
-    seq,
-    out_path,
-    img_shape=(2048, 1152),
-    fps=100,  # cam 10hz, 100fps = 10x real time
-    text_color=(0, 0, 0),
-):
-
-    cam_iter = seq.get_cam_left_iter()
-    cam_total = len(seq.camleft_frames)
-
-    writer = get_video_writer(out_path, img_shape, fps)
-
-    font = cv2.FONT_HERSHEY_TRIPLEX
-
-    for cam in tqdm(cam_iter, total=cam_total):
-
-        # Load data
-        utc_datetime = datetime.fromtimestamp(cam.timestamp, tz=timezone.utc)
-        name = utc_datetime.strftime("%Y/%m/%d %I:%M:%S%p")
-
-        # Plot
-        img_bgr = cv2.cvtColor(cam.img, cv2.COLOR_RGB2BGR)
-        cv2.putText(img_bgr, name, (10, 30), font, 1, text_color, 2, cv2.LINE_AA)
-
-        writer.write(img_bgr)
-
-    writer.release()
 
 
 def get_video_writer(out_path, img_shape, fps):
@@ -67,94 +37,108 @@ def get_video_writer(out_path, img_shape, fps):
     return writer
 
 
-def visualize_all_sensors_wrapper(frame_tuple):
-    cam, rad, lid, son = frame_tuple
-    return visualize_all_sensors(cam, rad, lid, son)
-
-
-def visualize_all_sensors(cam, rad, lid, son, save=None):
+def visualize_all_sensors(cam, rad, lid, son):
 
     cam.load_data()
     rad.load_data()
     lid.load_data()
     son.load_data()
 
-    utc_datetime = datetime.fromtimestamp(
-        cam.timestamp, tz=timezone(timedelta(hours=UTC_TO_EDT_OFFSET))
-    )
-    name = utc_datetime.strftime("%Y/%m/%d %I:%M:%S%p")
-
-    h1 = 1152
-    h2 = 1152
-    w1 = 2048
-    w2 = 1152
-    tot_img = np.zeros((h1 + h2, w1 + w2, 3), dtype=np.uint8)
-
     cam_img = cam.img  # (1152,2048,3)
-    tot_img[0:h1, 0:w1, :] = cam_img
+
+    h1 = cam_img.shape[0]
+    w1 = cam_img.shape[1]
+    h2 = h1
+    w2 = h2
 
     rad_img = dash_radar(  # (1152,1152)
         rad,
         cart_resolution=rad.resolution * 4,
-        cart_pixel_width=1152,
+        cart_pixel_width=w2,
     )
     rad_img_rgb = (np.tile(rad_img[:, :, np.newaxis], 3) * 255).astype(np.uint8)
-    tot_img[0:h1, w1 : w1 + w2, :] = rad_img_rgb
 
     son_img = dash_sonar(  # (1024,2048)
         son,
-        cart_pixel_height=2048 // 2,
+        cart_pixel_height=w1 // 2,  # Sonar: h=w/2=w1/2
         cart_resolution=son.resolution * 0.38,
     )
     son_height = son_img.shape[0]
     son_img_rgb = (np.tile(son_img[:, :, np.newaxis], 3) * 255).astype(np.uint8)
+
+    default_dpi = plt.rcParams["figure.dpi"]
+    lid_img = dash_lidar(
+        lid, figsize=(w2 / default_dpi, w2 / default_dpi), color="distance"
+    )  # (1152,152,3)
+
+    tot_img = np.zeros((h1 + h2, w1 + w2, 3), dtype=np.uint8)
+    tot_img[0:h1, 0:w1, :] = cam_img
+    tot_img[0:h1, w1 : w1 + w2, :] = rad_img_rgb
     tot_img[h1 : h1 + son_height, 0:w1, :] = son_img_rgb
-
-    lid_img = dash_lidar(lid, figsize=(11.52, 11.52), color="distance")  # (1152,152,3)
     tot_img[h2:, w1:, :] = lid_img
-
-    img_bgr = cv2.cvtColor(tot_img, cv2.COLOR_RGB2BGR)
-
-    cv2.putText(
-        img_bgr,
-        name,
-        (20, h1 + h2 - 20),
-        fontFace=cv2.FONT_HERSHEY_TRIPLEX,
-        color=(255, 255, 255),
-        fontScale=2.5,
-        thickness=2,
-    )
-
-    if save:
-        cv2.imwrite(save, img_bgr)
 
     return tot_img
 
 
-def write_dashboard_video(
-    seq,
-    out_path,
-    img_shape=(3200, 2304),
-    fps=100,  # cam 10hz, 100fps = 10x real time
-):
-    seq.synchronize_frames(ref="cam_left")
+def visualize_all_sensors_wrapper(frame_tuple):
+    cam, rad, lid, son = frame_tuple
 
+    return visualize_all_sensors(cam, rad, lid, son), cam.timestamp
+
+
+def visualize_cam_wrapper(frame_tuple):
+    (cam,) = frame_tuple
+
+    cam.load_data()
+    return cam.img, cam.timestamp
+
+
+def write_sequence_video(seq, out_path, fps=100, cam_only=False, utc_to_local=-5):
+
+    if cam_only:
+        frame_tuples = list(zip(seq.camleft_frames))
+        wrapper = visualize_cam_wrapper
+    else:
+        seq.synchronize_frames(ref="cam_left")
+        frame_tuples = list(
+            zip(
+                seq.camleft_frames, seq.radar_frames, seq.lidar_frames, seq.sonar_frames
+            )
+        )
+        wrapper = visualize_all_sensors_wrapper
+
+    img_shape = wrapper(frame_tuples[0])[0].shape[:2][::-1]  # dumb way to get (w,h)
     writer = get_video_writer(out_path, img_shape, fps)
-    total_frames = len(seq.camleft_frames)
+    total = len(frame_tuples)
 
-    frame_tuples = list(
-        zip(seq.camleft_frames, seq.radar_frames, seq.lidar_frames, seq.sonar_frames)
-    )
-
-    with ProcessPoolExecutor(max_workers=4) as executor:
-        for img in tqdm(
-            executor.map(visualize_all_sensors_wrapper, frame_tuples),
-            total=total_frames,
+    with Pool(max_workers=4) as executor:
+        for img, ts in tqdm(
+            executor.map(wrapper, frame_tuples),
+            total=total,
         ):
+
+            ts_string = ts_string_from_utc(ts, utc_to_local=utc_to_local)
+
+            cv2.putText(
+                img,
+                ts_string,
+                org=(20, img_shape[1] - 20),
+                fontFace=cv2.FONT_HERSHEY_TRIPLEX,
+                color=(255, 255, 255),
+                fontScale=2.5,
+                thickness=2,
+            )
+
             img_bgr = cv2.cvtColor(img, cv2.COLOR_RGB2BGR)
+
             writer.write(img_bgr)
 
     writer.release()
+
+
+def ts_string_from_utc(ts_sec, utc_to_local):
+    ts = datetime.fromtimestamp(ts_sec, tz=timezone(timedelta(hours=utc_to_local)))
+    return ts.strftime("%Y/%m/%d %I:%M:%S%p")
 
 
 def dash_lidar(
@@ -266,13 +250,9 @@ if __name__ == "__main__":
     seq = Sequence(canoe_path, [seq_id, seq_start, seq_end])
     seq.synchronize_frames(ref="cam_left")
 
-    for i in range(2):
-        cam = seq.camleft_frames[i]
-        son = seq.sonar_frames[i]
-        rad = seq.radar_frames[i]
-        lid = seq.lidar_frames[i]
-        visualize_all_sensors(cam, rad, lid, son, save=f"out-{i}.png")
-    write_dashboard_video(seq, out_path="sens-5.mp4")
-    # write_cam_left_video(seq, out_path="vid.mp4")
+    write_sequence_video(
+        seq, out_path="cam-5.mp4", cam_only=True, utc_to_local=UTC_TO_LOCAL
+    )
+    write_sequence_video(seq, out_path="sens-5.mp4", utc_to_local=UTC_TO_LOCAL)
 
     pass
